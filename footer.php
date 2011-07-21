26ca10e9ced6113791c9e1ce7041b4a0d18d3a7f
Josh Eaton <josh@jjeaton.com>
2011-07-20 21:42:06 -0500

    Added developer link to footer

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 
 <!-- ######### COPYRIGHT -->
 <div id="footer">
-<p><?php bloginfo('name'); ?> <?php _e('is powered by'); ?> <a href="http://www.wordpress.org/"><?php _e('Wordpress'); ?></a> <?php _e('and template by'); ?> <a href="http://www.wpdesigner.com/"><?php _e('WP Designer'); ?></a>. <?php wp_loginout(); ?></p>
+<p><?php bloginfo('name'); ?> <?php _e('is powered by'); ?> <a href="http://www.wordpress.org/"><?php _e('Wordpress'); ?></a>. <?php _e('Template by'); ?> <a href="http://www.wpdesigner.com/"><?php _e('WP Designer'); ?></a> <?php _e('and');?> <a href="http://www.jjeaton.com/"><?php _e('Josh Eaton'); ?></a>. <?php wp_loginout(); ?></p>
 <?php wp_footer(); ?>
 </div>
 <!-- ######### END COPYRIGHT -->
